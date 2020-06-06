dcf0e8887a4327022dee735c8d930a51bf69825f
Alexaltrex <alexaltrex@mail.ru>
2020-06-06 16:59:25 +0400

    header menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-$(function() {
-    $('.header__logo').on('mouseenter', ()=>{
+$(function () {
+    $('.header__logo').on('mouseenter', () => {
         $('.header__logohover').addClass('show');
     });
-    $('.header__logo').on('mouseleave', ()=>{
+    $('.header__logo').on('mouseleave', () => {
         $('.header__logohover').removeClass('show');
     });
+
+    // header__menu-item-notSelected
+    $('.header__menu-item').on('click', function () {
+
+        if ( !$(this).hasClass('header__menu-item-selected') ) {
+            let indexSelect = $('.header__menu-item').index($(this));
+            console.log(indexSelect);
+
+            $('.header__menu-item').each((index, item) => {
+                    if (index === indexSelect) {
+                        $(item).addClass('header__menu-item-selected');
+                        $(item).removeClass('header__menu-item-notSelected');
+                    } else if ($(item).addClass('header__menu-item-selected') ) {
+                        $(item).removeClass('header__menu-item-selected');
+                        $(item).addClass('header__menu-item-notSelected');
+                    }
+                }
+
+            );
+
+        }
+
+
+    })
 
 
 
